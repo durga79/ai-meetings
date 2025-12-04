d555846f1e605aabd25f48445ddac12d0f673bae
WexaAI <anas@wexa.ai>
2025-12-04 17:01:02 +0530

    refactor: remove unused UIKEY import from WelcomeScreen component
    Remove unused UIKEY type import from WelcomeScreen.tsx as it is no longer needed after previous refactoring.

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -3,7 +3,6 @@
 import React from "react";
 import { cn } from "@/lib/utils";
 import Logo from "./Logo";
-import { UIKEY } from "@/types";
 
 interface WelcomeScreenProps {
     className?: string;
